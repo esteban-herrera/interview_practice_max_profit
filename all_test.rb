4a9aa31d3f885373d6e33ae5668adadc76f274e9
Bulmaro Herrera <bherrera@bherrera-mac.local>
2016-07-27 21:45:35 -0700

    added full permutation test

---
+++ all_test.rb
@@ -0,0 +1,10 @@
+require "./MaxProfit.rb"
+
+[1,2,3,4,5].permutation.to_a.each do |p|
+	answer_brute = MaxProfit.find_max_profit(p)
+	answer_efficient = MaxProfit.find_max_profit_efficient(p)
+	if answer_efficient != answer_brute 
+		puts "Failed with array #{p.to_s}, expected:#{answer_brute} - actual:#{answer_efficient}"
+	end
+end
+                                                                                        
